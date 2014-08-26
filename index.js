2a87b2936aeda4f87c03731b8991fb28bd863972
Nicholas Crumrine <uncletammy@yahoo.com>
2014-08-26 18:56:20 -0500

    saves to mongo now

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // var server = require('http').Server(app);
 var mongo = require('mongoskin');
 var poleCam = require('camPiper');
+
 var db = mongo.db("mongodb://localhost:27017/images", {native_parser:true});
+var poleCamImages = db.collection('poleCam', {strict: false});
+
 var server = require('http').createServer(httpConnection);
 server.listen(1338);
 
@@ -12,35 +15,38 @@ var httpConnection = function(req,res){
 };
 
 
-var saveImage = function(data){
-	console.log('A new image ting of length:',typeof data);
+var saveImage = function(imageObject){
+	var getDate = new Date();
+	poleCamImages.insert({captureDate: getDate.getTime(),imgData:imageObject.imgData}, {w:1}, function(err,results){
+		if (err){
+			console.log('Error inserting image:',err);
+		} else {
+			console.log(getDate,' - Image Stored!');
+		}
+	});
 };
 
 // Set up an instance of CamPiper to rebroadcast the mjpeg stream to anyone who wants it.
 poleCam.name = 'chickenCam';
-poleCam.requestURL = 'http://70.113.19.166/image?speed=15';
+poleCam.requestURL = 'http://70.113.19.166/image?speed=5';
 poleCam.start();
 
 console.log('server is running');
 
-// var imageReceiver = require('imageReceiver')(server,'chickenCam','http://localhost:1339',null);
+var imageReceiver = require('imageReceiver')(server,'chickenCam','http://localhost:1339',null);
 
-// var imageSocket = require('socket.io-client')('http://localhost:1338');
-// 	imageSocket.on('connect', function(){
-// 		console.log('\n\n','Connected over sockets!!!','\n\n');
+var imageSocket = require('socket.io-client')('http://localhost:1338');
+	imageSocket.on('connect', function(){
+		console.log('\n\n','Connected over sockets!!!','\n\n');
 
-// 		imageSocket.on('speedTest',console.log);
-// 		imageSocket.on('newImage',saveImage);
+		// imageSocket.on('speedTest',console.log);
+		imageSocket.on('newImage',saveImage);
 
-// 		imageSocket.on('disconnect', function(){
-// 			console.log('Disconnected!')
-// 		});
-// });
-
-var ptzImages = db.collection('poleCam', {strict: true});
+		imageSocket.on('disconnect', function(){
+			console.log('Disconnected! at',(new Date()))
+		});
+});
 
 
-
-// ptzImages.create
 
 
